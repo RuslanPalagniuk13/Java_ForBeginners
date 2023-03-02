2182333a8307f4362d808c17738c8af1a1f95570
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-03-02 15:37:55 +0300

    Лекция 1 приветствие

---
+++ les1_Hello/lesson1.java
@@ -0,0 +1,9 @@
+/* Урок 1:
+ * Приветствие
+ */
+
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java"); // выводит на печать указанное в ковычках
+    }
+}
