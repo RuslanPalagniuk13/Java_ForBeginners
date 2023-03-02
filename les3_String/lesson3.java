80f0b125674bb5395e477302c71127ceab02b063
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-03-02 15:47:11 +0300

    Лекция 3 - строки

---
+++ les3_String/lesson3.java
@@ -0,0 +1,15 @@
+/* Урок 3:
+ * Строки (String)
+ */
+
+public class lesson3 {
+    public static void main(String[] args) {
+        int x = 13;
+        String s = "Привет"; // String относится к классу и пишится с большой боквы
+        String space = " ";
+        String name = "Ксюша";
+        System.out.println(s+space+name); // ввыводит строки по заданным переменным строкам
+        System.out.println("Привет" + " " + "Настя");
+        System.out.println("Мой номер " + x);
+    }
+}
