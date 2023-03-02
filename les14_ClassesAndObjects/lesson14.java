96d4940f94800b919ac4c99f431c47cef7871642
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-03-02 17:27:12 +0300

    Лекция 14 - классы и объекты

---
+++ les14_ClassesAndObjects/lesson14.java
@@ -0,0 +1,20 @@
+/* Урок 14:
+ * Классы и объекты
+ */
+
+public class lesson14 {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Роман";
+        person1.age = 20;
+        System.out.println("Меня зовут "+person1.name +", "+ "мне "+ person1.age+" лет");
+        Person person2 = new Person();
+        person2.name = "Иван";
+        person2.age = 50;
+        System.out.println("Меня зовут "+person2.name +", "+ "мне "+ person2.age+" лет");
+    }
+}
+class Person{
+    String name;
+    int age;
+}
