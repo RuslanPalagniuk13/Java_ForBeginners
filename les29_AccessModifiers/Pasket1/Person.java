2cb6e4ca3409fb4c51811628782a9518e9b5d787
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-03-07 13:32:24 +0300

    Лекция 29 - модификаторы доступа public, private, protected и default

---
+++ les29_AccessModifiers/Pasket1/Person.java
@@ -0,0 +1,9 @@
+package Pasket1;
+
+public class Person { // Доступ к папке Person внутри пакета
+    protected String name;
+
+    public Person(){
+        name = "Bob";
+    }
+}
---
+++ les29_AccessModifiers/Pasket1/lesson29.java
@@ -0,0 +1,8 @@
+package Pasket1;
+
+public class lesson29 {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        System.out.println(p1.name);
+    }
+}
