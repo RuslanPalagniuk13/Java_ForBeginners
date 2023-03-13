ffc761d0aba96dcaac83ab7c15454660385ce576
Руслан Палагнюк <rlpalagniuk@mail.ru>
2023-03-13 17:18:42 +0300

    Лекция 42 - Абстрактные классы

---
+++ les42_AbstractClasses/Animal.java
@@ -0,0 +1,11 @@
+// абстрактный класс - 
+
+public abstract class Animal{ 
+    public void eat(){
+        System.out.println("Animal is eting...");
+    }
+
+    public abstract void makeSound(); // абстрактный метод
+}
+
+    
